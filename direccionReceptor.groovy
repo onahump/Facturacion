a80cfc8dc973f27face11952b59aa6ebe1b7b3af
onahump <oswaldo@makingdevs.com>
2017-12-26 12:03:11 -0600

    Agregando el metodo darDireccion

---
+++ direccionReceptor.groovy
@@ -0,0 +1,17 @@
+
+
+class DireccionReceptor{
+  String calle
+  String numero
+  String codigoPostal
+  String municipioDelegacion
+  String estado
+  String pais
+
+  String darDireccion{
+    "${calle},${numero},${codigoPostal},${municipioDelegacion},${estado},${pais}"
+  }
+
+}
+
+
