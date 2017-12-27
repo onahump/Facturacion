0e48f73c71367eafea3b84f29413f7843688d5a9
onahump <oswaldo@makingdevs.com>
2017-12-27 11:42:12 -0600

    Tomando informacon del xml y agregandola a mapas

---
+++ factura.groovy
@@ -0,0 +1,67 @@
+ 
+def factura = new XmlSlurper().parse("archivo.xml")
+factura.declareNamespace(cfdi:'http://www.sat.gob.mx/cfd/3')
+
+def conceptos = [:]
+factura.Conceptos.'cfdi:Concepto'.each{ 
+	conceptos.put(it.'@descripcion',it.'@importe')
+}
+
+def receptor = [:]
+factura.Receptor.each{ 
+	receptor << [nombre:it.'@nombre',
+			   rfc:it.'@rfc']
+}
+
+def direccionEmisor = [:]
+factura.Receptor.'cfdi:Domicilio'.each{ 
+	direccionEmisor << [calle:it.'@calle',
+						noExterior:it.'@noExterior',
+						noInterior:it.'@noInterior',
+						codigoPostal:it.'@codigoPostal',
+						colonia:it.'@colonia',
+						estado:it.'@estado',
+						localidad:it.'@localidad',
+						pais:it.'@pais']
+}
+
+
+def emisor = [:]
+factura.Emisor.each{ 
+	emisor << [nombre:it.'@nombre',
+			   rfc:it.'@rfc']
+}
+
+def direccionFiscalEmisor = [:]
+factura.Emisor.'cfdi:DomicilioFiscal'.each{ 
+	direccionFiscalEmisor << [calle:it.'@calle',
+						noExterior:it.'@noExterior',
+						noInterior:it.'@noInterior',
+						codigoPostal:it.'@codigoPostal',
+						colonia:it.'@colonia',
+						estado:it.'@estado',
+						localidad:it.'@localidad',
+						pais:it.'@pais']
+}
+
+def direccionEmisorExpedida = [:]
+factura.Emisor.'cfdi:ExpedidoEn'.each{ 
+	direccionEmisorExpedida << [calle:it.'@calle',
+						noExterior:it.'@noExterior',
+						noInterior:it.'@noInterior',
+						codigoPostal:it.'@codigoPostal',
+						colonia:it.'@colonia',
+						estado:it.'@estado',
+						localidad:it.'@localidad',
+						pais:it.'@pais']
+}
+
+def iva = [:]
+factura.Impuestos.Traslados.'cfdi:Traslado'.each{ 
+	iva << [iva:it.'@importe',
+			tasa:it.'@tasa']
+}
+
+ivaEnte = iva.iva
+println ivaEnte.toInteger()
+
