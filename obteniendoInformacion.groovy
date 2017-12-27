b089c58f1afc93d1526ee504192401144d0c7f84
onahump <oswaldo@makingdevs.com>
2017-12-27 12:28:03 -0600

    Eliminando classe obteniendoInformacion

--- obteniendoInformacion.groovy
+++
@@ -1,5 +0,0 @@
-def leyendoFactura = new XmlParser().parse("archivo.xml")
-leyendoFactura.declareNamespace(cfdi:'http://www.sat.gob.mx/cfd/3')
-
-println leyendoFactura.Conceptos.'cfdi:Concepto'.each { println it.'@descripcion' }
-
