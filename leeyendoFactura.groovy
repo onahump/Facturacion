a810da07d47d53ee94a06305f81ecdb551befc08
onahump <oswaldo@makingdevs.com>
2017-12-29 19:08:35 -0600

    Convirtiendo en metodos el obtener mapas

--- leeyendoFactura.groovy
+++ leeyendoFactura.groovy
@@ -1,76 +1,89 @@
 class LeeyendoFactura{	
 
 	def xml = new File('archivo.xml')
-	
-	def readFactura(def archivo){
+	xml.declareNamespace(cfdi:'http://www.sat.gob.mx/cfd/3')
+
+
+	def readFactura(def factura){
 		def factura = new XmlSlurper().parse(archivo)
-		factura.declareNamespace(cfdi:'http://www.sat.gob.mx/cfd/3')
 		return factura 
-	}
+	}	
 
-	def lectura = readFactura(xml)
-
-	def getConceptos(){
-		def conceptos = [:]
-		factura.Conceptos.'cfdi:Concepto'.each{ 
-		conceptos.put(it.'@descripcion',it.'@importe')
+	def getConceptos(def readConcepto){
+		def conceptos_Mapa = [:]
+		readConcepto.Conceptos.'cfdi:Concepto'.each{ 
+		conceptos_Mapa.put(it.'@descripcion',it.'@importe')
 		}
+		return conceptos_Mapa
 	}
 
-	def receptor = [:]
-	factura.Receptor.each{ 
-		receptor << [nombre:it.'@nombre',
-				   rfc:it.'@rfc']
+	def getReceptor(def readReceptor){
+		def receptor_Mapa = [:]
+		readReceptor.Receptor.each{ 
+		receptor_Mapa << [nombre:it.'@nombre',
+					 rfc:it.'@rfc']
+		}
+		return receptor_Mapa
+	}
+		
+	def getEmisor(def readEmisor){
+		def emisor_Mapa = [:]
+		readEmisor.Emisor.each{ 
+			emisor_Mapa << [nombre:it.'@nombre',
+					   rfc:it.'@rfc']
+		}
+		return emisor_Mapa
 	}
 
-	def direccionEmisor = [:]
-	factura.Receptor.'cfdi:Domicilio'.each{ 
-		direccionEmisor << [calle:it.'@calle',
-							noExterior:it.'@noExterior',
-							noInterior:it.'@noInterior',
-							codigoPostal:it.'@codigoPostal',
-							colonia:it.'@colonia',
-							estado:it.'@estado',
-							localidad:it.'@localidad',
-							pais:it.'@pais']
+	def getIva(def readIva){
+		def iva_Mapa = [:]
+		readIva.Impuestos.Traslados.'cfdi:Traslado'.each{ 
+			iva_Mapa << [iva:it.'@importe',
+					tasa:it.'@tasa']
+		}
+		return iva_Mapa
 	}
 
-
-	def emisor = [:]
-	factura.Emisor.each{ 
-		emisor << [nombre:it.'@nombre',
-				   rfc:it.'@rfc']
+	def getDireccionEmisor(def readAddresEmisor){
+		def direccionEmisor = [:]
+		readAddres.Emisor.'cfdi:Domicilio'.each{ 
+			direccionEmisor << [calle:it.'@calle',
+								noExterior:it.'@noExterior',
+								noInterior:it.'@noInterior',
+								codigoPostal:it.'@codigoPostal',
+								colonia:it.'@colonia',
+								estado:it.'@estado',
+								localidad:it.'@localidad',
+								pais:it.'@pais']
+		}
+		return direccionEmisor
+	}
+	
+	def getDireccionReceptor(def readAddresReceptor){
+		def direccionReceptor = [:]
+		readAddresReceptor.Receptor.'cfdi:Domicilio'.each{ 
+			direccionReceptor << [calle:it.'@calle',
+								noExterior:it.'@noExterior',
+								noInterior:it.'@noInterior',
+								codigoPostal:it.'@codigoPostal',
+								colonia:it.'@colonia',
+								estado:it.'@estado',
+								localidad:it.'@localidad',
+								pais:it.'@pais']
+		}
+		return direccionReceptor
 	}
 
-	def direccionFiscalEmisor = [:]
-	factura.Emisor.'cfdi:DomicilioFiscal'.each{ 
-		direccionFiscalEmisor << [calle:it.'@calle',
-							noExterior:it.'@noExterior',
-							noInterior:it.'@noInterior',
-							codigoPostal:it.'@codigoPostal',
-							colonia:it.'@colonia',
-							estado:it.'@estado',
-							localidad:it.'@localidad',
-							pais:it.'@pais']
-	}
+	def newFactura = readFactura(xml)
+	def conceptosDeFactura = getConceptos(newFactura)
+	def recepor = getReceptor(newFactura)
+	def emisor = getEmisor(newFactura)
+	def iva = getIva(newFactura)
+	def direccionEmisor = getDireccionEmisor(newFactura)
+	def direccionReceptor = getDireccionReceptor(newFactura)
 
-	def direccionEmisorExpedida = [:]
-	factura.Emisor.'cfdi:ExpedidoEn'.each{ 
-		direccionEmisorExpedida << [calle:it.'@calle',
-							noExterior:it.'@noExterior',
-							noInterior:it.'@noInterior',
-							codigoPostal:it.'@codigoPostal',
-							colonia:it.'@colonia',
-							estado:it.'@estado',
-							localidad:it.'@localidad',
-							pais:it.'@pais']
-	}
 
-	def iva = [:]
-	factura.Impuestos.Traslados.'cfdi:Traslado'.each{ 
-		iva << [iva:it.'@importe',
-				tasa:it.'@tasa']
-	}
+
 }
 
 
