75105b940df7ba5b4019ebaebda02966835345d7
Oswaldo Nahum Ponce Arriaga <oswaldo@makingdevs.com>
2018-01-03 16:49:26 -0600

    Cambiando la instancia y haciendo refactor a la clase main

--- main.groovy
+++ main.groovy
@@ -1,6 +1,4 @@
-listaDeFacturas = []
-
-Direccion direccionEmisor = new Direccion(
+Direccion direccionDelEmisor = new Direccion(
 	calle:"Rodolfo Gaona", 
 	noExterior:"86",
 	noInterior:"E",
@@ -9,8 +7,8 @@ Direccion direccionEmisor = new Direccion(
 	estado:"CDMX",
 	municipio:"Coyoacan",
 	pais:"México")
-Emisor emisor1 = new Emisor(razonSocial:"LA EUROPEA MEXICO, S.A.P.I DE C.V.",rfc:"EME910610G1A")
-emisor1.direccion = direccionEmisor
+Emisor emisor = new Emisor(razonSocial:"LA EUROPEA MEXICO, S.A.P.I DE C.V.",rfc:"EME910610G1A")
+emisor.direccion = direccionDelEmisor
 
 Direccion direccionReceptor = new Direccion(
 	calle:"Calzada Ermita Iztapalapa",
@@ -21,9 +19,9 @@ Direccion direccionReceptor = new Direccion(
 	estado:"CDMX",
 	municipio:"Iztapalapa",
 	pais:"México")
-Receptor receptor1 = new Receptor(razonSocial:"MAKING DEVS",
+Receptor receptor = new Receptor(razonSocial:"MAKING DEVS",
 	rfc:"EME910610G1A")
-receptor1.direccion = direccionReceptor
+receptor.direccion = direccionReceptor
 
 Concepto concepto1 = new Concepto(cantidad:1, descripcion:"Coca-cola 135ml",importe:372.267241)
 Concepto concepto2 = new Concepto(cantidad:2, descripcion:"Chetos",importe:249.301724)    
@@ -31,30 +29,16 @@ def listaDeConceptos = [concepto1,concepto2]
 
 //Instanciando la factura numero 1 y agregando elementos a sus atributos"
 Factura factura1 = new Factura(
-							   emisor:emisor1, 
-							   receptor:receptor1, 
+							   emisor:emisor, 
+							   receptor:receptor, 
 							   conceptos:listaDeConceptos)
-factura1.subtotal = factura1.subtotal
-factura1.iva = factura1.iva
-factura1.total = factura1.total
 
 
 //Imprimiendo la factura
-s = """\
 
-${'*'*26} FACTURA ${'*'*26}
+println factura1
 
-Nombre del Emisor: ${factura1.emisor}
-Nombre del Receptor: ${factura1.receptor}
-Concepto(s)
-Cantidad: ${factura1.conceptos[0].cantidad}  Descripcion: ${factura1.conceptos[0].descripcion} Importe: ${factura1.conceptos[0].importe}
-Cantidad: ${factura1.conceptos[1].cantidad}  Descripcion: ${factura1.conceptos[1].descripcion} 	  Importe: ${factura1.conceptos[1].importe}
+//Factura factura2 = algunaObjetoConAlgunMEtodo.obtenFactura(50)
 
-				  Subtotal: \$ ${factura1.subtotal}
-			                 Iva: \$ ${factura1.iva}
-				    Total: \$ ${factura1.total}
-
-${'*'*52}
-"""
-println s
-
+//println factura2
+println new Factura()
