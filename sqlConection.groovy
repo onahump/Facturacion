a5428bddab79c9a932a356be2eab7c2a697467fb
Oswaldo Nahum Ponce Arriaga <oswaldo@makingdevs.com>
2018-01-03 12:55:38 -0600

    Ejecutando valores

--- sqlConection.groovy
+++ sqlConection.groovy
@@ -20,7 +20,7 @@ class SqlConection {
 
   void insertandoIntoTable(String valores){
   	//String comandoInsert = " insert into factura (fecha,nombre_del_emisor, nombre_del_receptor, subtotal, iva, total) values ${valores}"
-  	sql.execute("insert into factura (fecha, nombre_del_emisor, nombre_del_receptor, subtotal, iva, total) values ${valores}")
+  	sql.execute(valores)
   }
 
 
