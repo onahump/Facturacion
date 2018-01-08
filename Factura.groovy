18435a35532de0731bea255936c652d8afa5be78
Oswaldo Nahum Ponce Arriaga <oswaldo@makingdevs.com>
2018-01-08 11:36:08 -0600

    Eliminando el metodo toString

--- Factura.groovy
+++ Factura.groovy
@@ -1,7 +1,7 @@
 class Factura{
 	Date fecha = new Date()
-	Emisor emisor
-	Receptor receptor
+	InvoiceEntity emisor
+	InvoiceEntity receptor
 	List<Concepto> conceptos = []
 
 	String getFechaConFormato(){
@@ -18,25 +18,6 @@ class Factura{
 
 	Double getTotal(){
 		subtotal + iva
-	}
-
-	String toString(){
-		"""\
-
-		${'*'*26} ${fechaConFormato} ${'*'*26}
-
-		Nombre del Emisor: ${emisor}
-		Nombre del Receptor: ${receptor}
-		Concepto(s)
-		${conceptos[0]}
-		${conceptos[1]}
-
-						  Subtotal: \$ ${subtotal}
-					                 Iva: \$ ${iva}
-						    Total: \$ ${total}
-
-		${'*'*65}
-		"""
 	}
 
 }
