69fb0051e291e6d1adf146014aef53809f25eacb
Oswaldo Nahum Ponce Arriaga <oswaldo@makingdevs.com>
2018-01-11 12:50:32 -0600

    cambiando los nombres realacionandolos con la base

--- ContadorPublico.groovy
+++ ContadorPublico.groovy
@@ -8,17 +8,21 @@ class ContadorPublico{
 	def db = [user:"root", pass:'makingdevs',url:'jdbc:mysql://localhost/facturas', driver:'com.mysql.jdbc.Driver']
 	def sql = Sql.newInstance(db.url, db.user, db.pass, db.driver)	
 
-	String registraLaFactura(def factura){
-		String comandoInsert = "insert into factura (fecha, nombre_del_emisor, emisor_rfc, nombre_del_receptor, receptor_rfc, subtotal, iva, total) values ('${factura.fecha.format('yyyy-dd-MM')}', '${factura.emisor.razonSocial}', '${factura.emisor.rfc}', '${factura.receptor.razonSocial}', '${factura.receptor.rfc}', '${factura.subtotal}', '${factura.iva}', '${factura.total}')"
-  		sql.execute(comandoInsert)
-	}
+	String registraLaFactura(Factura factura){
+		String insertandoFactura = "insert into factura (fecha, emisor, rfc_emisor, receptor, rfc_receptor, subtotal, iva, total) values ('${factura.fecha.format('yyyy-dd-MM')}', '${factura.emisor.razonSocial}', '${factura.emisor.rfc}', '${factura.receptor.razonSocial}', '${factura.receptor.rfc}', '${factura.subtotal}', '${factura.iva}', '${factura.total}')"
+  		sql.execute(insertandoFactura)
+  		/*factura.conceptos.each{ 
+            sql.execute("insert into concepto (cantidad, descripcion, importe) values ('${it.cantidad}','${it.descripcion}', '${it.importe}')");
+   	  		
+        }*/
+  	}
 
 	Factura quieroLaFactura(def value){
-		String consultaPorId = "select * from factura WHERE id = ${value}"
+		String consultaPorId = "select * from factura WHERE cve_factura = ${value}"
 		def parseo = sql.rows(consultaPorId)
 
-		InvoiceEntity emisor =  new InvoiceEntity(razonSocial:parseo.nombre_del_emisor.first(), rfc:parseo.emisor_rfc.first() )
-		InvoiceEntity receptor =  new InvoiceEntity(razonSocial:parseo.nombre_del_receptor.first(),rfc:parseo.receptor_rfc.first() )
+		InvoiceEntity emisor =  new InvoiceEntity(razonSocial:parseo.emisor.first(), rfc:parseo.rfc_emisor.first() )
+		InvoiceEntity receptor =  new InvoiceEntity(razonSocial:parseo.receptor.first(),rfc:parseo.rfc_receptor.first() )
 		
 		Concepto concepto2 = new Concepto(cantidad:1, 
 								  		  descripcion:"Chetos",
