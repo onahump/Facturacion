8c4a4aeaeeff65fa1de391cb5a1a920782efa6e3
Oswaldo Nahum Ponce Arriaga <oswaldo@makingdevs.com>
2018-01-15 16:10:47 -0600

    #1 Pintando la factura solicitada

--- ContadorPublico.groovy
+++ ContadorPublico.groovy
@@ -11,25 +11,29 @@ class ContadorPublico{
 	String registraLaFactura(Factura factura){
 		String insertandoFactura = "insert into factura (fecha, emisor, rfc_emisor, receptor, rfc_receptor) values ('${factura.fecha.format('yyyy-MM-dd')}', '${factura.emisor.razonSocial}', '${factura.emisor.rfc}', '${factura.receptor.razonSocial}', '${factura.receptor.rfc}')"
   		sql.execute(insertandoFactura)
+  		
   		def ultimoIdRegistrado = sql.rows("SELECT LAST_INSERT_ID()")
         String parseoDelUltimoIdRegistrado = ultimoIdRegistrado
        	String id = parseoDelUltimoIdRegistrado.findAll(/\d+/).first()
+  		
   		factura.conceptos.each{ 
             sql.execute("insert into concepto (cantidad, descripcion, importe,cve_factura) values ('${it.cantidad}','${it.descripcion}', '${it.importe}', '${id}')")
         }
   	}
 
 	Factura quieroLaFactura(def value){
-		String consultaPorId = "select * from factura WHERE cve_factura = ${value}"
-		def parseo = sql.rows(consultaPorId)
+		String consultaFacturaPorId = "select * from factura WHERE cve_factura = ${value}"
+		def parseo = sql.rows(consultaFacturaPorId)
 
 		InvoiceEntity emisor =  new InvoiceEntity(razonSocial:parseo.emisor.first(), rfc:parseo.rfc_emisor.first() )
 		InvoiceEntity receptor =  new InvoiceEntity(razonSocial:parseo.receptor.first(),rfc:parseo.rfc_receptor.first() )
-		
-		Concepto concepto2 = new Concepto(cantidad:1, 
-								  		  descripcion:"Chetos",
-								  		  importe:12.13)  
-		def listaDeConceptos = [concepto2]
+
+		def listaDeConceptos = []
+
+		sql.eachRow("select cantidad,descripcion,importe from concepto where cve_factura = ${value}"){
+	    	def concepto = new Concepto(cantidad:it.cantidad, descripcion:it.descripcion, importe:it.importe)
+	    	listaDeConceptos << concepto
+    	}
 
 		Factura factura = new Factura(fecha:parseo.fecha.first(),
 								  emisor:emisor,
