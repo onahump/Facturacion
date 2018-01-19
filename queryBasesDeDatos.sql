1d32a1477b0fe67bb4d28617b498d8d464929962
Oswaldo Nahum Ponce Arriaga <oswaldo@makingdevs.com>
2018-01-19 12:29:51 -0600

    Modificando el query de creacion de las tablas

--- queryBasesDeDatos.sql
+++ queryBasesDeDatos.sql
@@ -1,12 +1,23 @@
-CREATE TABLE IF NOT EXISTS factura(
-cve_factura INT NOT NULL auto_increment,
-fecha date NOT NULL,
-emisor  varchar(45) NOT NULL,
-rfc_emisor  varchar(45) NOT NULL,
-receptor  varchar(45) NOT NULL,
-rfc_receptor  varchar(45) NOT NULL,
-PRIMARY KEY(cve_factura)
-)
+CREATE TABLE IF NOT EXISTS factura(                         
+cve_factura int(11) NOT NULL AUTO_INCREMENT, 
+fecha date NOT NULL,                         
+emisor varchar(45) NOT NULL,                 
+rfc_emisor varchar(45) NOT NULL,             
+receptor varchar(45) NOT NULL,               
+rfc_receptor varchar(45) NOT NULL,           
+PRIMARY KEY (cve_factura)                    
+) 
+
+CREATE TABLE IF NOT EXISTS concepto (                                                                      |
+cve_concepto int(5) NOT NULL AUTO_INCREMENT,                                               
+cantidad int(5) NOT NULL,                                                                  
+descripcion varchar(50) NOT NULL,                                                          
+importe decimal(10,2) NOT NULL,                                                               
+cve_factura int(11) NOT NULL DEFAULT '0',                                                  
+PRIMARY KEY (cve_concepto),                                                                
+KEY fk_cve_factura (cve_factura),                                                        
+CONSTRAINT fk_cve_factura FOREIGN KEY (cve_factura) REFERENCES factura (cve_factura) 
+) 
 
 CREATE TABLE IF NOT EXISTS concepto(
 cve_concepto INT(5) NOT NULL auto_increment,
@@ -17,8 +28,6 @@ cve_factura int NOT NULL,
 PRIMARY KEY(cve_concepto)
 )
 
-
--- Ver como se crea la tabla
 show create table concepto;
 -- Eliminando una llave foranea de una tabla donde "concepto_ibfk_1" se obtiene de table concepto
 ALTER TABLE concepto DROP FOREIGN KEY concepto_ibfk_1 
