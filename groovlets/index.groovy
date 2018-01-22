ef288e3a4f1c621e3c77bd7129c1048545a919bd
Oswaldo Nahum Ponce Arriaga <oswaldo@makingdevs.com>
2018-01-22 11:10:20 -0600

    #4 Carpeta para el almacenamiento de los groovlets

---
+++ groovlets/index.groovy
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title></title>
+	</head>
+	<body>
+	<body>
+		<h1>
+		   Bienvenido al sistema de Facturacion de ${organizacion} !!!
+		</h1>
+		<p>Selecciona una de las opciones</p>
+	</body>
+	</body>
+</html>
