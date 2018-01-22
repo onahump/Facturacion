64aeaa6243d7ebe0c7748e7765ec954d1687a8c1
onahump <oswaldo@makingdevs.com>
2018-01-21 20:56:25 -0600

    #3 creando el index.groovy

---
+++ index.groovy
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title></title>
+	</head>
+	<body>
+		<h1>
+		  Hola estimado ${nombre} !!!
+		</h1>
+		<h2>
+		  Fue bueno verte en el curso  de <%= if(organizacion) organizacion else "MakingDevs" %>
+		</h2>
+	</body>
+</html>
