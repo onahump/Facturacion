1fba8ac4ad94214b76b1095095162bc9decc4fd1
Oswaldo Nahum Ponce Arriaga <oswaldo@makingdevs.com>
2018-01-22 17:34:57 -0600

    #4 Creando la pantalla de inicio del groovlet

--- groovlets/index.groovy
+++ groovlets/index.groovy
@@ -1,15 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-	<head>
-		<meta charset="UTF-8">
-		<title></title>
-	</head>
-	<body>
-	<body>
-		<h1>
-		   Bienvenido al sistema de Facturacion de ${organizacion} !!!
-		</h1>
-		<p>Selecciona una de las opciones</p>
-	</body>
-	</body>
-</html>
+Templates pagina = new Templates()
+
+println pagina.generaUnTemplateParaRenderear("index")
