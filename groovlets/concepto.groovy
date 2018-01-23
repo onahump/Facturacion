fc5a58f09de6dc31d40756818297853c1419e790
Oswaldo Nahum Ponce Arriaga <oswaldo@makingdevs.com>
2018-01-23 11:02:03 -0600

    #4 Creando el groovlet de factura

---
+++ groovlets/concepto.groovy
@@ -0,0 +1,10 @@
+Templates pagina = new Templates()
+	
+	Concepto concepto = new Concepto(cantidad:params.cantidad.toInteger(), 
+									  descripcion:params.descripcion,
+									  importe:params.importe.toBigDecimal())
+
+	System.out.println(concepto)
+
+println pagina.generaUnTemplateParaRenderear("concepto")
+
