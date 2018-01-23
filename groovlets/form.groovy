4e35e7e59fa97ffaf73be6589279bc5f5bce2796
Oswaldo Nahum Ponce Arriaga <oswaldo@makingdevs.com>
2018-01-23 11:02:25 -0600

    #4 Eliminando los conceptos

--- groovlets/form.groovy
+++ groovlets/form.groovy
@@ -1,3 +1,25 @@
 Templates pagina = new Templates()
 
+	InvoiceEntity emisor = new InvoiceEntity(razonSocial:params.nombre_emisor,
+							   rfc:params.rfc_emisor)
+
+	InvoiceEntity receptor = new InvoiceEntity(razonSocial:params.nombre_receptor,
+							   rfc:params.rfc_receptor)
+
+	Concepto concepto1 = new Concepto(cantidad:1, 
+									  descripcion:"Tachas 100g",
+									  importe:25.56)
+	Concepto concepto2 = new Concepto(cantidad:1, 
+									  descripcion:"Coca-ina 250g",
+									  importe:100.50)   
+
+	def listaDeConceptosFactura = [concepto1,concepto2]
+
+	Factura factura = new Factura(emisor:emisor, 
+								   receptor:receptor, 
+								   conceptos:listaDeConceptosFactura)
+
+	System.out.println(factura)
+
 println pagina.generaUnTemplateParaRenderear("form")
+
