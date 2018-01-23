70786806f1763819ffde6ee5f9a1559ff614adf5
Oswaldo Nahum Ponce Arriaga <oswaldo@makingdevs.com>
2018-01-23 11:20:22 -0600

    #4 agregando inicios de seciones

--- groovlets/concepto.groovy
+++ groovlets/concepto.groovy
@@ -1,9 +1,12 @@
+
+
 Templates pagina = new Templates()
 	
-	Concepto concepto = new Concepto(cantidad:params.cantidad.toInteger(), 
+	Concepto concepto = new Concepto(cantidad:params.cantidad.toInteger(),
 									  descripcion:params.descripcion,
 									  importe:params.importe.toBigDecimal())
 
+	
 	System.out.println(concepto)
 
 println pagina.generaUnTemplateParaRenderear("concepto")
