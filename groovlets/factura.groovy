367205a2cc376811c29fd044416f75e62ca11207
Oswaldo Nahum Ponce Arriaga <oswaldo@makingdevs.com>
2018-01-29 15:22:22 -0600

    #4 creando el objeto factura y mapeando el objeto

--- groovlets/factura.groovy
+++ groovlets/factura.groovy
@@ -8,6 +8,13 @@ if (!session.counter) {
 	session.counter += 1
 }
 
+if(!session.factura){
+	session.factura = new Factura(emisor:session.emisor,
+								  receptor:session.receptor,
+								  conceptos:session.listaDeConceptos)
+}
+
 Template index = new Template()
 
-println index.generaUnaVista("factura")
+println index.generaUnaVista("factura",[factura:session.factura])
+
