c34f4744cdf1f9e83438d0efdd8bdc91dce6415e
Oswaldo Nahum Ponce Arriaga <oswaldo@makingdevs.com>
2018-01-29 12:49:21 -0600

    #15 Enviando informacion de un emisor y receptor a la vista de concepto

--- groovlets/registro.groovy
+++ groovlets/registro.groovy
@@ -1,3 +1,22 @@
+if (!session) {
+	session = request.getSession(true)
+}
+
+if (!session.counter) {
+	session.counter = 1
+} else {
+	session.counter += 1
+}
+
+
+println params
+println "<br><br>"
+println context.properties
+println "<br><br>"
+println session?.properties
+println "<br><br>"
+println "Contador de sesiones: ${session.counter}"
+
 Template registro = new Template()
 
-println registro.generaUnaVista("registro",[:])
+println registro.generaUnaVista("registro")
