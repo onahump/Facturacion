035528406542971236589ffcee91afa74c23d520
Oswaldo Nahum Ponce Arriaga <oswaldo@makingdevs.com>
2018-01-29 15:59:12 -0600

    #11 Registrando la factura en la base de datos y nulificando las sesiones

--- groovlets/factura.groovy
+++ groovlets/factura.groovy
@@ -13,6 +13,18 @@ if(!session.factura){
 }
 
 Template index = new Template()
-
 println index.generaUnaVista("factura",[factura:session.factura])
+
+							//Llamando a yarey 
+ContadorPublico yarey = new ContadorPublico()
+//Yarey verifica que existan las tablas en la base de datos 
+yarey.verificaSiLasTablasExisten()
+//Yarey por favor registra la factura.
+yarey.registraLaFactura(session.factura)
+
+session.factura = null
+session.receptor = null
+session.listaDeConceptos = null
+session.receptor = null
+session.concepto = null
 
