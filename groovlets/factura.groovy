d3df0e77542d848802339b8aa34a80935ef5f665
Oswaldo Nahum Ponce Arriaga <oswaldo@makingdevs.com>
2018-02-07 11:31:59 -0600

    Descomentando a yarey

--- groovlets/factura.groovy
+++ groovlets/factura.groovy
@@ -16,11 +16,11 @@ Template index = new Template()
 println index.generaUnaVista("factura",[factura:session.factura])
 
 							//Llamando a yarey 
-//ContadorPublico yarey = new ContadorPublico()
+ContadorPublico yarey = new ContadorPublico()
 //Yarey verifica que existan las tablas en la base de datos 
-//yarey.verificaSiLasTablasExisten()
+yarey.verificaSiLasTablasExisten()
 //Yarey por favor registra la factura.
-//yarey.registraLaFactura(session.factura)
+yarey.registraLaFactura(session.factura)
 
 session.factura = null
 session.receptor = null
