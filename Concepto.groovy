b0e38f5159695059f11f77369dec487839e063bc
Oswaldo Nahum Ponce Arriaga <oswaldo@makingdevs.com>
2018-02-09 16:34:06 -0600

    #26 Agregando metodo para obtener el importetotal

--- Concepto.groovy
+++ Concepto.groovy
@@ -3,7 +3,11 @@ class Concepto{
 	String descripcion 
 	BigDecimal importe
 
+	BigDecimal getImporteTotal(){
+		cantidad * importe
+	}
+
 	String toString(){
-	  "Cantidad: ${cantidad}  Descripcion: ${descripcion} Importe: ${importe}"
+	  "Cantidad: ${cantidad}  Descripcion: ${descripcion} Importe: ${importeTotal}"
 	}
 }
