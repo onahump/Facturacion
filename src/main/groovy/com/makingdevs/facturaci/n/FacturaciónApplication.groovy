3c88d75242a601f6a1edc569ee5b0b21afb4bdf4
Oswaldo Nahum Ponce Arriaga <oswaldo@makingdevs.com>
2018-03-07 11:38:01 -0600

    #27 Adding Spring boot in the project

---
+++ src/main/groovy/com/makingdevs/facturaci/n/FacturaciónApplication.groovy
@@ -0,0 +1,12 @@
+package com.makingdevs.facturaci.n
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class FacturaciónApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run FacturaciónApplication, args
+	}
+}
---
+++ src/test/groovy/com/makingdevs/facturaci/n/FacturaciónApplicationTests.groovy
@@ -0,0 +1,16 @@
+package com.makingdevs.facturaci.n
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner)
+@SpringBootTest
+class FacturaciónApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
