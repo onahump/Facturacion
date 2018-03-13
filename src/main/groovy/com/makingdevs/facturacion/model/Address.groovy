536854141bf095a92efceff7d160a5db232c800b
onahump <oswaldo@makingdevs.com>
2018-03-12 22:50:20 -0600

    #31 Creating Address model

---
+++ src/main/groovy/com/makingdevs/facturacion/model/Address.groovy
@@ -0,0 +1,16 @@
+package com.makingdevs.facturacion.model
+
+import 
+
+class Address{
+
+	@NotNull
+	String street
+	String num_exterior
+	String num_interior
+	String neighborhood
+	String city
+	String state 
+	String country
+
+}
