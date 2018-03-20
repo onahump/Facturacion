fcea61c9c6e37a7b8f86fdb98df9aa09dafc5c09
onahump <oswaldo@makingdevs.com>
2018-03-19 20:10:30 -0600

    #31 Modifying the address model

--- src/main/groovy/com/makingdevs/facturacion/model/Address.groovy
+++ src/main/groovy/com/makingdevs/facturacion/model/Address.groovy
@@ -1,16 +1,42 @@
 package com.makingdevs.facturacion.model
 
-import 
+import javax.persistence.Entity
+import javax.persistence.Table
+import javax.persistence.Id
+import javax.persistence.GeneratedValue
+import javax.persistence.GenerationType
+import javax.persistence.Column
 
+
+@Entity
+@Table(name = "Address")
 class Address{
 
-	@NotNull
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "ID_ADDRESS")
+	Integer id
+
+	@Column(name = "STREET")
 	String street
+
+	@Column(name = "NUM_EXTERIOR")
 	String num_exterior
+	
+	@Column(name = "NUM_INTERIOR")
 	String num_interior
+	
+	@Column(name = "NEIGHBORHOOD")
 	String neighborhood
+	
+	@Column(name = "CITY")
 	String city
+	
+	@Column(name = "STATE")
 	String state 
+	
+	@Column(name = "COUNTRY")
 	String country
+
 
 }
