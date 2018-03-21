13307c9b90a8db17d323cff7225d66d272213874
Oswaldo Nahum Ponce Arriaga <oswaldo@makingdevs.com>
2018-03-21 11:15:14 -0600

    #37 Creating the Invoice Entity service implementation

---
+++ src/main/groovy/com/makingdevs/facturacion/service/InvoiceEntityServiceImpl.groovy
@@ -0,0 +1,36 @@
+package com.makingdevs.facturacion.service
+
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Override
+import org.springframework.stereotype.Service
+
+import com.makingdevs.facturacion.model.Address
+import com.makingdevs.facturacion.repository.InvoiceEntityRepository
+
+@Service
+class InvoiceEntityServiceImpl implements InvoiceEntityService{
+
+	@Autowired
+	InvoiceEntityRepository invoiceEntityRepository
+
+	@Override
+	Iterable<InvoiceEntity> listAllInvoiceEntities(){
+		invoiceEntityRepository.findAll()
+	}
+
+	@Override
+	InvoiceEntity getInvoiceEntityById(Integer id){
+		invoiceEntityRepository.findOne(id)
+	}
+
+	@Override
+	InvoiceEntity saveInvoiceEntity(InvoiceEntity invoiceEntity){
+		invoiceEntityRepository.save(invoiceEntity)
+	}
+
+	@Override
+	InvoiceEntity deleteInvoiceEntity(Integer id){
+		invoiceEntityRepository.delete(id)
+	}
+
+}
