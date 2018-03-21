b4f654b7e4d34809bb8bbe84685969ab69413299
Oswaldo Nahum Ponce Arriaga <oswaldo@makingdevs.com>
2018-03-21 10:42:46 -0600

    #28 Addiing JPA annotations and relationship OneToOne

--- src/main/groovy/com/makingdevs/facturacion/model/InvoiceEntity.groovy
+++ src/main/groovy/com/makingdevs/facturacion/model/InvoiceEntity.groovy
@@ -1,1 +1,33 @@
-InvoiceEntity.groovy
+package com.makingdevs.facturacion.model
+
+import javax.persistence.Id
+import javax.persistence.GeneratedValue
+import javax.persistence.GenerationType
+import javax.persistence.Table
+import javax.persistence.Column
+import javax.persistence.Entity
+import javax.persistence.CascadeType
+import javax.persistence.FetchType
+import javax.persistence.OneToOne
+import javax.persistence.JoinColumn
+
+
+@Entity
+@Table(name="INVOICE_ENTITY")
+class InvoiceEntity{
+	
+	@Id
+	@GeneratedValue(strategy= GenerationType.AUTO)
+	@Column(name="ID_INVOICE_ENTITY")
+	Interger id
+
+	@Column(name="BUSSISNESS_NAME")
+	String bussisnesName
+	
+	@Column(name="RFC")
+	String rfc
+
+	@OneToOne( cascade = CascadeType.ALL, fetch= FetchType.EAGER)
+  	@JoinColumn(name="ADDRESS_ID")
+	Address address
+}
