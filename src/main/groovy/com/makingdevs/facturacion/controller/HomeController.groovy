106ae5fa3955cbaa409a01e96d22cf70051c8fea
Oswaldo Nahum Ponce Arriaga <oswaldo@makingdevs.com>
2018-03-23 13:25:35 -0600

    #42 Creating Home controller

---
+++ src/main/groovy/com/makingdevs/facturacion/controller/HomeController.groovy
@@ -0,0 +1,21 @@
+package com.makingdevs.facturacion.controller
+
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestMethod
+
+import com.makingdevs.facturacion.service.InvoiceEntityService
+import com.makingdevs.facturacion.model.Address
+import com.makingdevs.facturacion.model.InvoiceEntity
+
+@Controller
+class HomeController{
+	
+	@RequestMapping("/")
+    String homeControleer(){
+    	"index"
+    }
+}
