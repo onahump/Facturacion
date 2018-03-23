9fe890c2fcb531874da28413c1d022d617f3d42a
Oswaldo Nahum Ponce Arriaga <oswaldo@makingdevs.com>
2018-03-23 13:01:32 -0600

    #41 Creating a new method to add a new InoviceEntity

---
+++ src/main/groovy/com/makingdevs/facturacion/controller/InvoiceEntityController.groovy
@@ -0,0 +1,32 @@
+package com.makingdevs.facturacion.controller
+
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestMethod
+
+import com.makingdevs.facturacion.service.InvoiceEntityService
+import com.makingdevs.facturacion.model.Address
+import com.makingdevs.facturacion.model.InvoiceEntity
+
+@Controller
+class InvoiceEntityController{
+
+	@Autowired
+	AddressService addressService
+	
+	@Autowired
+	InvoiceEntityService invoiceEntityService 
+	
+	@RequestMapping("InvoiceEntity/new")
+    String newInvoiceEntity(Model model){
+    	model.addAttribute("invoiceEntity",address:new Address(),
+    					   invoiceEntity:new InvoiceEntity())
+    	"invoiceEntityForm"
+    }
+
+    @RequestMapping()
+
+}
